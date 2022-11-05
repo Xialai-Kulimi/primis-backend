8ff97b529016d59f808682686c432bbefca4d2ba
Kulimi <isilkoodarze@gmail.com>
2022-11-05 21:35:05 +0800

    Update backend/__main__.py
    remove auth

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,11 +1,15 @@
+from os import getenv
+
 from fastapi import Depends, FastAPI
 
-from backend.dependencies import get_token_header
-from backend.routers import entity, ws, auth
+from backend.dependencies import get_cookie_or_token
+from backend.routers import entity, ws
+from starlette.middleware.sessions import SessionMiddleware
 
-app = FastAPI(dependencies=[Depends(get_token_header)])
 
+app = FastAPI(dependencies=[Depends(get_cookie_or_token)])
+
+app.add_middleware(SessionMiddleware, secret_key=getenv("OAUTH2_CLIENT_SECRET"))
 
 app.include_router(entity.router)
 app.include_router(ws.router)
-app.include_router(auth.router)

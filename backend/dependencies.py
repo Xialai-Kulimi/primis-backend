57adf857bc89ed047e94d393a054496ab84f7ee3
Kulimi <isilkoodarze@gmail.com>
2022-11-05 21:57:13 +0800

    Update backend/dependencies.py

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -2,6 +2,6 @@ from backend.utils.console import console
 from fastapi import Request
 
 
-async def get_discord_id(request: Request):
+async def login(request: Request):
     console.log(request.session['discord_id'])
     return request.session

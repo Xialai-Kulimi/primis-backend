e3679f413d03c53271d635ddbad9c7107219ebc9
Kulimi <isilkoodarze@gmail.com>
2022-11-05 21:37:48 +0800

    Update backend/__main__.py
    Change depend name

--- backend/__main__.py
+++ backend/__main__.py
@@ -2,12 +2,12 @@ from os import getenv
 
 from fastapi import Depends, FastAPI
 
-from backend.dependencies import get_cookie_or_token
+from backend.dependencies import get_discord_id
 from backend.routers import entity, ws
 from starlette.middleware.sessions import SessionMiddleware
 
 
-app = FastAPI(dependencies=[Depends(get_cookie_or_token)])
+app = FastAPI(dependencies=[Depends(get_discord_id)])
 
 app.add_middleware(SessionMiddleware, secret_key=getenv("OAUTH2_CLIENT_SECRET"))
 

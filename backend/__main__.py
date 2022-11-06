f8d689d113927c8a6d42f57d9e55044516dff674
Kulimi <isilkoodarze@gmail.com>
2022-11-06 22:56:01 +0800

    Update backend/__main__.py
    Restructure import and add controller.main loop

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,15 +1,20 @@
 from os import getenv
 
 from fastapi import Depends, FastAPI
+from starlette.middleware.sessions import SessionMiddleware
 
 from backend.dependencies import login
 from backend.routers import entity, ws
-from starlette.middleware.sessions import SessionMiddleware
-
+from backend.controller import controller
 
 app = FastAPI(dependencies=[Depends(login)])
-
 app.add_middleware(SessionMiddleware, secret_key=getenv("OAUTH2_CLIENT_SECRET"))
-
 app.include_router(entity.router)
 app.include_router(ws.router)
+
+
+import asyncio
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(controller.main.main())
+    

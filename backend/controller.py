c38e89356b8716e47021fc3cc29eb06f4504a7ad
Kulimi <isilkoodarze@gmail.com>
2022-11-06 22:57:19 +0800

    Update backend/controller.py
    Change to import by path, and as package

--- backend/controller.py
+++ backend/controller.py
@@ -1,11 +1,4 @@
-import importlib.util
 import sys
 import os
-
-
-file_path = os.getenv('CONTROLLER_PATH')
-module_name = "controller"
-spec = importlib.util.spec_from_file_location(module_name, file_path)
-controller = importlib.util.module_from_spec(spec)
-sys.modules[module_name] = controller
-spec.loader.exec_module(controller)
+sys.path.append(os.getenv('CONTROLLER_PATH'))
+import controller

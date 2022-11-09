5a9b59d0edf9b18f9afddc0f61b503c57e63f720
Kulimi <isilkoodarze@gmail.com>
2022-11-10 01:12:30 +0800

    test: test session exist

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,7 +1,11 @@
 from backend.utils.console import console
-from fastapi import Request
+from typing import Union
+from fastapi import Cookie, Depends, FastAPI, Query, WebSocket, status, Request
 
+async def login(session: Union[str, None]= Cookie(default=None)):
+    # console.print(session)
+    # console.log(session)
+    return session
 
-async def login(request: Request):
-    console.log(request.session['discord_id'])
-    return request.session
+async def check_have_discord_id(request: Request):
+    return request.session.get("session", None)

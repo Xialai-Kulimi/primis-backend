9538c7a61e45792b4b401f4264dc9b76ca592d62
Kulimi <isilkoodarze@gmail.com>
2022-11-10 01:12:14 +0800

    test: test session

--- backend/routers/entity.py
+++ backend/routers/entity.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends, Request
 
+from backend.dependencies import check_have_discord_id
+from backend.utils.console import console
 
 router = APIRouter(
     prefix="/api/entity",
     tags=["entity"],
+
     responses={404: {"description": "Not found"}},
 )
 
@@ -17,7 +20,9 @@ async def read_items():
 
 
 @router.get("/{item_id}")
-async def read_item(item_id: str):
+async def read_item(item_id: str, request: Request):
+    console.log(request.session.get('discord_id'))
+
     if item_id not in fake_items_db:
         raise HTTPException(status_code=404, detail="Item not found")
     return {"name": fake_items_db[item_id]["name"], "item_id": item_id}
@@ -27,7 +32,8 @@ async def read_item(item_id: str):
     "/{item_id}",
     responses={403: {"description": "Operation forbidden"}},
 )
-async def update_item(item_id: str):
+async def update_item(item_id: str, request: Request):
+    console.log(request.session.get('discord_id'))
     if item_id != "plumbus":
         raise HTTPException(
             status_code=403, detail="You can only update the item: plumbus"

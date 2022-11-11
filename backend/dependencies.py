61547ce8f5695f6e1ab5be731b40544aa00b54cd
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-11 23:51:41 +0800

    feat: add get_token_or_session

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -9,3 +9,12 @@ async def login(session: Union[str, None]= Cookie(default=None)):
 
 async def check_have_discord_id(request: Request):
     return request.session.get("session", None)
+
+async def get_cookie_or_token(
+    websocket: WebSocket,
+    session: Union[str, None] = Cookie(default=None),
+    token: Union[str, None] = Query(default=None),
+):
+    if session is None and token is None:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token

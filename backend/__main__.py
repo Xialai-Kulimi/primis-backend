79affc8bc60e3ba540566a51c246b83581b363b1
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-12 00:48:10 +0800

    style: move import

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,20 +1,19 @@
 from os import getenv
+import asyncio
 
 from fastapi import Depends, FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 
 from backend.dependencies import login
-from backend.routers import entity, ws
+from backend.routers import ws
 from backend.controller import controller
 
 app = FastAPI(dependencies=[Depends(login)])
-app.add_middleware(SessionMiddleware, secret_key=getenv("OAUTH2_CLIENT_SECRET"))
-app.include_router(entity.router)
+app.add_middleware(SessionMiddleware,
+                   secret_key=getenv("OAUTH2_CLIENT_SECRET"))
 app.include_router(ws.router)
 
 
-import asyncio
 @app.on_event("startup")
 async def startup_event():
     asyncio.create_task(controller.main.main())
-    

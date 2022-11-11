a5257d7ccf3950dbddbec74b419d500145d9448f
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-12 01:24:44 +0800

    fix: upadte os env

--- backend/utils/mongo.py
+++ backend/utils/mongo.py
@@ -1,5 +1,5 @@
 from os import getenv
 from pymongo import MongoClient
 
-mgclient = MongoClient(getenv('DB_CONNECT'))
+mgclient = MongoClient(getenv('MONGO_DB_CONNECT'))
 global_db = mgclient[getenv('GLOBAL_DB_NAME', default='global')]

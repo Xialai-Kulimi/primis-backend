305c9c9d4056c5fdbce486be7b63edd601b81634
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-12 14:02:59 +0800

    feat: use the new depend

--- backend/routers/ws.py
+++ backend/routers/ws.py
@@ -4,25 +4,27 @@ from datetime import datetime
 
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, HTTPException, Depends
 
-from backend.utils.console import console
-from backend.controller import controller
-
-from backend.dependencies import get_cookie_or_token
+from backend.dependencies import get_token
 from backend.auth import User, ClientInfo, Client
 
+from backend.controller import controller
+console = controller.utils.console
 manager = controller.manager.manager
 handler = controller.handler.handler
 
 
 router = APIRouter(
-    prefix="/api/ws",
     tags=["ws"],
     responses={404: {"description": "Not found"}},
 )
 
+# @router.get("/api/ws")
+# async def api_ws_test(cookie_or_token: str = Depends(get_token())):
+#     console.log(cookie_or_token)
+#     return HTTPException(status_code=404)
 
-@router.websocket("/")
-async def websocket_endpoint(websocket: WebSocket, cookie_or_token: str = Depends(get_cookie_or_token)):
+@router.websocket("/api/ws")
+async def websocket_endpoint(websocket: WebSocket, token: str = Depends(get_token)):
     """websocket entry endpoint
 
     1. validate the token
@@ -33,14 +35,13 @@ async def websocket_endpoint(websocket: WebSocket, cookie_or_token: str = Depend
 
     Args:
         websocket (WebSocket): websocket (wss://)
-        cookie_or_token (str, optional): should be token
+        token (str): should be token
 
     Raises:
         HTTPException: when the token is invalid
     """
 
-
-    client_info = ClientInfo(token=cookie_or_token)
+    client_info = ClientInfo(token=token)
     client = Client(websocket, client_info)
     if not client.user.valid:
         raise HTTPException(status_code=403, detail="invalid token")

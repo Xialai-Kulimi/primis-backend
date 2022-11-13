41c1b55fe2c48c471c0215c9f581a53f605f581b
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-13 16:38:32 +0800

    perf: remove unused functions

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,20 +1,13 @@
+
+from fastapi import Cookie, WebSocket, status
+
 from backend.utils.console import console
-from typing import Union
-from fastapi import Cookie, Depends, FastAPI, Query, WebSocket, status, Request
 
-async def login(session: Union[str, None]= Cookie(default=None)):
-    # console.print(session)
-    # console.log(session)
-    return session
-
-async def check_have_discord_id(request: Request):
-    return request.session.get("session", None)
 
 async def get_token(
     websocket: WebSocket = None,
     token: str | None = Cookie(default=None),
 ):
-    console.log(token)
     if token is None:
         await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
     return token
